e22f6ae044a8f11a2becd9a2ea68a7b9118a3bb4
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2024-01-26 20:42:05 +0200

    Add files via upload

---
+++ 13,12(2)/13,12(2)/13,12(2).cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <cstring>
+
+class MyString {
+private:
+    char* str;
+    int size;
+    static int count;
+
+public:
+    MyString() : str(new char[80]), size(80) {
+        count++;
+    }
+
+    MyString(int length) : str(new char[length]), size(length) {
+        count++;
+    }
+
+    MyString(const char* s) : str(new char[strlen(s) + 1]), size(strlen(s)) {
+        strcpy(str, s);
+        count++;
+    }
+
+    void inputString() {
+        std::cout << "Enter a string: ";
+        std::cin.getline(str, size);
+    }
+
+    void displayString() const {
+        std::cout << "String: " << str << std::endl;
+    }
+
+    ~MyString() {
+        delete[] str;
+        count--;
+    }
+
+    static int getCount() {
+        return count;
+    }
+};
+
+int MyString::count = 0;
+
+int main() {
+    MyString defaultString;
+    defaultString.displayString();
+
+    MyString customSizeString(50);
+    customSizeString.inputString();
+    customSizeString.displayString();
+
+    MyString initializedString("Hello, World!");
+    initializedString.displayString();
+
+    std::cout << "Number of MyString objects created: " << MyString::getCount() << std::endl;
+
+    return 0;
+}
---
+++ 13,12/13,12/13,12.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <string>
+
+class Contact {
+private:
+    std::string* fullName;
+    std::string homePhone;
+    std::string workPhone;
+    std::string mobilePhone;
+    std::string additionalInfo;
+
+public:
+    Contact(const std::string& name, const std::string& home, const std::string& work,
+        const std::string& mobile, const std::string& additional)
+        : fullName(new std::string(name)), homePhone(home), workPhone(work),
+        mobilePhone(mobile), additionalInfo(additional) {}
+
+    ~Contact() {
+        delete fullName;
+    }
+
+    const std::string& getFullName() const {
+        return *fullName;
+    }
+
+    const std::string& getHomePhone() const {
+        return homePhone;
+    }
+
+    const std::string& getWorkPhone() const {
+        return workPhone;
+    }
+
+    const std::string& getMobilePhone() const {
+        return mobilePhone;
+    }
+
+    const std::string& getAdditionalInfo() const {
+        return additionalInfo;
+    }
+
+    void setFullName(const std::string& name) {
+        *fullName = name;
+    }
+
+    void setHomePhone(const std::string& home) {
+        homePhone = home;
+    }
+
+    void setWorkPhone(const std::string& work) {
+        workPhone = work;
+    }
+
+    void setMobilePhone(const std::string& mobile) {
+        mobilePhone = mobile;
+    }
+
+    void setAdditionalInfo(const std::string& additional) {
+        additionalInfo = additional;
+    }
+};
+
+int main() {
+    Contact contact("Иван Иванов", "123456789", "987654321", "999888777", "Дополнительная информация");
+
+    std::cout << "Имя: " << contact.getFullName() << std::endl;
+    std::cout << "Домашний телефон: " << contact.getHomePhone() << std::endl;
+    std::cout << "Рабочий телефон: " << contact.getWorkPhone() << std::endl;
+    std::cout << "Мобильный телефон: " << contact.getMobilePhone() << std::endl;
+    std::cout << "Дополнительная информация: " << contact.getAdditionalInfo() << std::endl;
+
+    contact.setFullName("Петр Петров");
+    contact.setMobilePhone("111222333");
+
+    std::cout << "\nИзмененные данные:" << std::endl;
+    std::cout << "Имя: " << contact.getFullName() << std::endl;
+    std::cout << "Мобильный телефон: " << contact.getMobilePhone() << std::endl;
+
+    return 0;
+}
